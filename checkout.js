0b38956d52adbb3a6f749173fb6a6b0eb1ba225f
coxcachristo04 <elfabulosocr7@gmail.com>
2025-12-30 16:50:10 -0600

    TEST PAYPAL 2

--- checkout.js
+++ checkout.js
@@ -144,6 +144,7 @@ document.addEventListener("DOMContentLoaded", () => {
         return actions.order.create({
           purchase_units: [{
             //amount: { value: total.toFixed(2) }
+            currency_code: "MXN",
             value: '5.00'
           }]
         });
